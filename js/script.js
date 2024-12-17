9a344feab00c0f2756f473bffef56c4c5b4e23d1
Matteo Denis <matteo.denis@ensta-paris.fr>
2024-12-17 01:08:53 +0100

    Selection of genre working

--- js/script.js
+++ js/script.js
@@ -83,23 +83,50 @@ map_countries2iso = {
     "brazil": "Brazil",
 }
 
-// map_detailed2broadgenres
+map_detailed2broadgenres = {
+    'Alt. Rock': 'Rock',
+    'Blues': 'Rock',
+    'Country': 'Other',
+    'Disco': 'EDM',
+    'EDM': 'EDM',
+    'Folk': 'Other',
+    'Funk': 'Other',
+    'Gospel': 'Other',
+    'Jazz': 'Other',
+    'Metal': 'Rock',
+    'Pop': 'Pop',
+    'Punk': 'Rock',
+    'R&B': 'HipHop',
+    'Rap': 'HipHop',
+    'Reggae': 'Other',
+    'Rock': 'Rock',
+    'SKA': 'Other',
+    'Today': 'Other',
+}
 
 
 function createViz() {
     console.log("Using D3 v" + d3.version);
-    // createWorldmap();
-    loadData();
+    loadData("All");
 }
 
 
-function loadData() {
+function loadData(genre) {
+    files_to_load = ["data/ClassicHit_raw.csv"];
+    let AllGenres = true;
+    if (genre === "All") {
+        geo2load = "data/GeoGenre_cleaned.csv";
+        AllGenres = true;
+    } else {
+        geo2load = `data/Geo${map_detailed2broadgenres[genre]}.csv`;
+        AllGenres = false;
+    }
     Promise.all([
         d3.csv("data/ClassicHit_raw.csv", d3.autoType),
-        d3.csv("data/GeoGenre_cleaned.csv", d3.autoType)
-    ]).then(function(data) { 
+        d3.csv(geo2load, d3.autoType)
+    ]).then(function(data) {
 
-        let [classichit, geogenre] = data; 
+        let [classichit, geogenre] = data;
 
         const parseYear = d3.timeParse("%Y");
         classichit.forEach(d => {
@@ -110,18 +137,26 @@ function loadData() {
         classichit = classichit.filter((d) => d.Year < parseYear("2024"));
         classichit = classichit.filter((d) => d.Genre !== "World");
 
-        // ctx.classichit = classichit;
-        // ctx.geogenre = geogenre;
+        if (genre !== "All") {
+            classichit = classichit.filter((d) => d.Genre === genre);
+        }
 
         console.log("Data loaded:", classichit, geogenre);
 
-        createTimeline(classichit);
-        createWorldmap(geogenre);
-        createCarac(classichit);
+        Promise.all([
+            createTimeline(classichit),
+            createWorldmap(geogenre, !AllGenres),
+            createCarac(classichit),
+        ]).then(() => {window.scrollTo({top: 0, behavior: 'smooth'});})
+
+        // setTimeout(() => {
+        //     window.scrollTo({top: 0, behavior: 'smooth'});
+        // }, 1000);
 
     }).catch(function(error) {
         console.error("Error loading the CSV file:", error);
     });
+
 };
 
 
@@ -129,9 +164,11 @@ function createTimeline(classichit) {
     const containerWidth = document.getElementById("TimelineDiv").clientWidth;
     const containerHeight = window.innerHeight;
 
+    d3.select("#WorldmapDiv").selectAll("*").remove();
+
     const vlSpec = {
         "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-        "data": {"values": classichit}, 
+        "data": {"values": classichit},
         "mark": "bar",
         "width": 0.7 * containerWidth,
         "height": 0.7 * containerHeight,
@@ -147,27 +184,26 @@ function createTimeline(classichit) {
                 "title": "Number of Songs",
                 "type": "quantitative"
             },
+            // "color": {
+            //     "field": "Genre",
+            //     "type": "nominal"
+            // }
             "color": {
-                "field": "Genre",
-                "type": "nominal"
-            }
+                "aggregate": "count",
+                "title": "Number of Songs",
+                "type": "quantitative",
+                "scale": {"scheme": "greens"}
+            },
         }
     };
-    const vlOpts = {actions: false}; 
+    const vlOpts = {actions: false};
     vegaEmbed("#TimelineDiv", vlSpec, vlOpts);
 }
 
 
-function createWorldmap(geogenre) {
+function createWorldmap(geogenre, show) {
     const containerWidth = document.getElementById("WorldmapDiv").clientWidth;
     const containerHeight = window.innerHeight;
-
-    let genre_map = {};
-    geogenre.forEach(d => {
-        genre_map[map_countries2iso[d.Country]] = d.Pop;
-    });
-
-    const color = d3.scaleSequential([0, 1], d3.interpolateReds).unknown("lightgrey");
 
     d3.json("data/countries-50m.json").then(function(world) {
 
@@ -180,56 +216,72 @@ function createWorldmap(geogenre) {
             .append("svg")
             .attr("width", containerWidth)
             .attr("height", containerHeight);
-        
+
         svgEl.append("path") // Sphere path
             .datum({type: "Sphere"})
             .attr("fill", d3.color("white"))
             .attr("stroke", "lightgrey")
             .attr("d", geoPathGen);
 
-        svgEl.append("g") // Countries path
+        let countriesG = svgEl.append("g") // Countries path
             .attr("id", "countriesG")
             .selectAll("path")
             .data(countries.features)
             .join("path")
             .attr("d", geoPathGen)
             .attr("fill", "lightgrey")
-            .attr("fill", d => color(genre_map[d.properties.name]))
-            .attr("stroke", "white");
+            // .attr("fill", d => color(genre_map[d.properties.name]))
+            .attr("stroke", "lightgrey");
 
-        // legend
-        let range4legend = d3.range(0, 1, .01).reverse();
-        let scale4legend = d3.scaleLinear()
-            .domain([0, 1])
-            .rangeRound([range4legend.length, 0]);
-        let legendG = svgEl.append("g")
-            .attr("id", "legendG")
-            .attr("transform", `translate(${0.75*containerWidth}, 100)`);
-        legendG.selectAll("line")
-            .data(range4legend)
-            .enter()
-            .append("line")
-            .attr("x1", 0)
-            .attr("y1", (d, j) => (j))
-            .attr("x2", 20)  //ctx.STRIP_H = 20
-            .attr("y2", (d, j) => (j))
-            .attr("stroke", (d) => (color(d)));
-        legendG.append("g")
-            .attr("transform", `translate(${20 + 4},0)`) //ctx.STRIP_H
-            .call(d3.axisRight(scale4legend).ticks(5));
-        legendG.append("text")
-            .attr("x", 50)
-            .attr("y", range4legend.length / 2)
-            .style("fill", "black")
-            .attr("font-size", "11px")
-            .attr("font", "Helvetica Neue")
-            .text("Proportion de musiques \"Pop\" dans le Top 50"); //Genre
+
+        if (show) {
+            let genre_map = {};
+            geogenre.forEach(d => {
+                genre_map[map_countries2iso[d.Country]] = d.popularity;
+            });
+
+            let domainExtent = d3.extent(geogenre, (d) => d.popularity)
+            let color = d3.scaleSequential(domainExtent, d3.interpolateGreens).unknown("lightgrey"); 
+
+            countriesG.attr("fill", d => color(genre_map[d.properties.name]));
+            createMapLegend(svgEl, containerWidth, color, domainExtent); //Genre
+        }
 
     }).catch(function(error) {
         console.error("Error loading the GeoJSON file:", error);
     });
 }
 
+
+function createMapLegend(svgEl, containerWidth, color, domainExtent) {
+    
+    let range4legend = d3.range(domainExtent[0], domainExtent[1], (domainExtent[1]-domainExtent[0])/100).reverse();
+    let scale4legend = d3.scaleLinear()
+        .domain(domainExtent)
+        .rangeRound([range4legend.length, 0]);
+    let legendG = svgEl.append("g")
+        .attr("id", "legendG")
+        .attr("transform", `translate(${0.75 * containerWidth}, 100)`);
+    legendG.selectAll("line")
+        .data(range4legend)
+        .enter()
+        .append("line")
+        .attr("x1", 0)
+        .attr("y1", (d, j) => (j))
+        .attr("x2", 20) //ctx.STRIP_H = 20
+        .attr("y2", (d, j) => (j))
+        .attr("stroke", (d) => (color(d)));
+    legendG.append("g")
+        .attr("transform", `translate(${20 + 4},0)`) //ctx.STRIP_H
+        .call(d3.axisRight(scale4legend).ticks(5));
+    legendG.append("text")
+        .attr("x", 50)
+        .attr("y", range4legend.length / 2)
+        .style("fill", "black")
+        .attr("font-size", "11px")
+        .attr("font", "Helvetica Neue")
+        .text("Proportion de musiques \"Pop\" dans le Top 50");
+}
 
 function createCarac(classichit) {
     const containerWidth = document.getElementById("TimelineDiv").clientWidth;
@@ -273,7 +325,22 @@ function createCarac(classichit) {
 
 
 
-// Fonction clic
+// function pour ramener en haut de la page sur changement de genre dans le select:
+// de manière smooth
+
+function changeGenre() {
+    let genre = document.getElementById("genreSelect").value;
+    loadData(genre);
+}
+
+
+
+
+
+
+
+
+// Fonction pour descendre d'une section (après appui sur la flèche):
 
 // function clic(prochain) {
 //     var arrows = document.querySelectorAll(".down");
@@ -287,10 +354,10 @@ function createCarac(classichit) {
 //     var top = document
 //       .querySelector("body")
 //       .getBoundingClientRect().top; /*distance jusqu'au de la page*/
-  
+
 //     var haut = 0;
 //     var id = setInterval(frame, 10);
-  
+
 //     function frame() {
 //       if (haut < top) {
 //         clearInterval(id);
@@ -307,14 +374,14 @@ function createCarac(classichit) {
 //     var distance = document.querySelector(prochain).getBoundingClientRect().top;
 //     var bas = 0;
 //     var n = Math.floor(distance / 10);
-  
+
 //     var id1 = setInterval(frame1, 10);
-  
+
 //     function frame1() {
 //       if (bas >= n * 10) {
 //         clearInterval(id1);
 //         var id2 = setInterval(frame2, 10);
-  
+
 //         function frame2() {
 //           if (bas >= distance) {
 //             clearInterval(id2);
